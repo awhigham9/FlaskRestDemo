f66567e861c874d1e2e24ed0bedc8451e67740c5
Andrew Whigham <awhigham9@gmail.com>
2021-11-17 13:34:42 -0500

    Add script to init database from csv

---
+++ initdb.py
@@ -0,0 +1,41 @@
+''' Basic Script to initialize the SQLite3 db
+    with the correct data
+'''
+import sqlite3
+import csv
+import os
+
+CSV_PATH = 'disney_movies.csv'
+DB_PATH = 'movies.sqlite3'
+
+def main():
+    if os.path.exists(DB_PATH):
+        print("Removing existing db. . .")
+        os.remove(DB_PATH)
+
+    conn = sqlite3.connect(DB_PATH)
+
+    conn.execute('''
+        CREATE TABLE movies
+        (
+            title TEXT,
+            release_date TEXT,
+            genre TEXT,
+            mpaa_rating TEXT,
+            total_gross INTEGER,
+            inflation_adjusted_gross INTEGER
+        )
+    ''')
+
+    with open(CSV_PATH, 'r') as f:
+        print('Loading csv. . .')
+        reader = csv.reader(f)
+        next(reader, None) # Skip the header
+        print('Inserting data into db. . .')
+        conn.executemany('INSERT INTO movies values (?,?,?,?,?,?)', reader)
+        print('Committing transaction. . .')
+        conn.commit()
+
+
+if __name__ == '__main__':
+    main()
