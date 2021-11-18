935f8fc2631a8d91685460984a0fcd1ba0f1609c
root <root@DESKTOP-R7MUAPV.localdomain>
2021-11-18 17:06:28 -0500

    working GET requests from frontend

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
-function query() {
-    var title = document.querySelector('input').value;
-    fetch('/movie/title?=')
-    alert(title);
+var base = 'http://127.0.0.1:5000';
+function hello() {
+    console.log(`${base}/hello`);
+    axios.get(`${base}/hello`)
+    .then(res => console.log(res.data))
+    .catch(err => console.log(err));
+}
+
+async function query() {
+    var list = document.querySelector('ul');
+    var title = document.querySelector('#title').value;
+    await fetch(`${base}/movies/${title}`)
+    .then(res => res.text())
+    .then(data => {
+        console.log(data);
+        list.insertAdjacentHTML('afterBegin', `<li> ${data} </li>`);
+    })
+    .catch(err => console.log(err));
 }
 
