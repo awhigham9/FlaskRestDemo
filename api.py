a7dff065ba260f9a334a21eb908eda6e005ba28b
Andrew Whigham <awhigham9@gmail.com>
2021-11-17 20:16:57 -0500

    Add POST request

--- api.py
+++ api.py
@@ -93,6 +93,45 @@ class Movies(Resource):
         # Return the updated entry w/ 200 OK
         return self.get(title)
 
+    def post(self, title):
+        '''HTTP POST Request'''
+        # Input validation
+        cur = self.get_db_cursor()
+        cur.execute('SELECT * FROM movies WHERE title=:title', {'title' : title})
+        if cur.fetchall():
+            # Found a movie with matching title -> 400 Bad Request
+            abort(400, message="Movie with title already exists")
+
+        cleanForm = {}
+        for col in self.database_columns.keys():
+            if col not in request.form:
+                cleanForm[col] = self.database_columns[col]() # Default of type
+            else:
+                value = request.form[col]
+                if type(value) is not self.database_columns[col]:
+                    abort(400, message="Invalid form data") # Wrong type
+                cleanForm[col] = value
+        cleanForm['title'] = title
+
+        # Query database
+        conn = sqlite3.connect(self.db_path)
+        cur = conn.cursor()
+        try:
+            cur.execute(
+                '''
+                INSERT INTO movies
+                (title, release_date, genre, mpaa_rating, total_gross, inflation_adjusted_gross)
+                VALUES
+                (:title, :release_date, :genre, :mpaa_rating, :total_gross, :inflation_adjusted_gross)
+                ''',
+                cleanForm
+            )
+        except sqlite3.Error as e:
+            return self.abort_on_exception(str(e))
+        conn.commit()
+
+        return self.get(title), 201 # 201 Created
+
 api.add_resource(HelloWorld, '/hello')
 api.add_resource(Movies, '/movies/<string:title>')
 
